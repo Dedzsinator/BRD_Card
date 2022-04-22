55b8679a870d8c6b5227ebddd54de340c77e11e8
Dégi Nándor <degi.nandor@gmail.com>
2022-04-23 00:59:02 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import  Main  from './components/main/main';
+import  Cards  from './components/cards/cards';
+import  Signup  from './components/signup/signup';
+import  AddCard  from './components/addcard/addcard';
+import Pay from './components/pay/pay';
+import Statement from './components/statement/statement';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return(
+    <Router>
+      <Route path="/" exact component={Main}/>
+      <Route path="/signup" exact component={Signup}/>
+      <Route path="/cards" exact component={Cards}/>
+      <Route path="/addcard" exact component={AddCard}/>
+      <Route path="/pay" exact component={Pay}/>
+      <Route path="/statement" exact component={Statement}/>
+    </Router>
+    )
 }
-
 export default App;
